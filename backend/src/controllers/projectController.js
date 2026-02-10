6586c3b03565504ef5f6191d87dc03e11c67b164
Kauan Plaza <kauanbgs13@gmail.com>
2026-02-10 14:17:50 -0300

    server nao roda mas o banco foi

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,5 +1,24 @@
-const connect = require("../connect");
+const prisma = require("../connect")
 
 module.exports = class projectController {
-  
-};
+  static async createProject(req, res) {
+    const { title } = req.body;
+    if (!title) {
+      return res
+        .status(400)
+        .json({ error: "Todos os campos devem ser preenchidos" });
+    }
+    const query = `INSERT INTO project (title) VALUES (?)`
+    value = [title]
+    try {
+        prisma.query(query, value, function(err, results)){
+        if (err) {
+          console.log(err);
+        }
+        return res.status(200).json("OK!")
+        }} catch {
+            console.log(err)
+            return res.status(500).json("NAO OK")
+    }
+}
+}
