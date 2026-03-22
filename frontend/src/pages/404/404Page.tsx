8ceb278465bdbfffecb3fe52b090d8305050669e
Kauan Borges <kauanbgs13@gmail.com>
2026-03-22 00:33:03 -0300

    fix to build, link import removed

--- frontend/src/pages/404/404Page.tsx
+++ frontend/src/pages/404/404Page.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 export default function Page404() {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-white text-black font-secondary">
