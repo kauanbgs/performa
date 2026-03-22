87b9601226dfe59bc92c7cae4488e244a32a55b0
Kauan Borges <kauanbgs13@gmail.com>
2026-03-22 00:21:57 -0300

    fix: baseURL to real backendURL

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/performa",
+    baseURL: "https://performa-c5w1.onrender.com/performa",
 });
 
 const sheets = {
